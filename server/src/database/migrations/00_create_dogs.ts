71da719e1df168e526e6da19bf47441caaad5fef
Rafael Benvindo <rafael.benvindo@trt1.jus.br>
2021-10-28 22:10:27 -0300

    Versão inicial
    Cria migration da tabela.
    
    Define configurações inicias do projeto.

---
+++ server/src/database/migrations/00_create_dogs.ts
@@ -0,0 +1,13 @@
+import {Knex} from 'knex';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('dogs', table => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('gender').notNullable();
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('dogs');
+}
